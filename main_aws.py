a1175eed4cbe30e9227e9afba19c338f5f24eba3
prakrit-ti <prakrit.raj@trilogy.com>
2023-02-07 11:02:55 +0000

    sample_code

---
+++ main_aws.py
@@ -0,0 +1,38 @@
+import boto3
+
+cf_client = boto3.client('cloudformation', region_name='us-west-1')
+
+template_body = """
+---
+AWSTemplateFormatVersion: '2010-09-09'
+Resources:
+  MyBucket:
+    Type: AWS::S3::Bucket
+    Properties:
+      BucketName: s3-demo-bucket
+    Tags:
+      - Key: Owner
+        Value: prakrit.raj@trilogy.com
+      - Key: Project
+        Value: Inclusion Index
+      - Key: Quarter
+        Value: Q1-2023
+"""
+cf_client.create_stack(
+    StackName='s3-demo-stack',
+    TemplateBody=template_body,
+    Tags=[
+        {
+            'Key': 'Owner',
+            'Value': 'prakrit.raj@trilogy.com'
+        },
+        {
+            'Key': 'Project',
+            'Value': 'Inclusion Index'
+        },
+        {
+            'Key': 'Quarter',
+            'Value': 'Q1-2023'
+        }
+    ]
+)
